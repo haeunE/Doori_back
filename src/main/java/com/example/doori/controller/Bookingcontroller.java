38a6e24c0b1896cff4e025b0135a1c80816ddbc3
hana <simhana0122@gmail.com>
2024-11-21 10:02:14 +0900

    metadate수정

--- src/main/java/com/example/doori/controller/Bookingcontroller.java
+++ src/main/java/com/example/doori/controller/Bookingcontroller.java
@@ -3,6 +3,7 @@ package com.example.doori.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,12 @@ public class Bookingcontroller {
 		
 		bookingService.saveBooking(bookingDTO);
 		return new ResponseEntity<>("예약완료 되었습니다", HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/reservation/seats")
+	public ResponseEntity<?> reservedSeat(){
+		
 	}
 
 }
