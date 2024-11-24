f5bc946a1bc584c5a7aa4ade085cc54ebd9400ef
hana <simhana0122@gmail.com>
2024-11-25 00:47:16 +0900

    리뷰 저장, 그리고 내리뷰 보이기 가능

--- src/main/java/com/example/doori/repository/ReviewRepository.java
+++ src/main/java/com/example/doori/repository/ReviewRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.doori.domain.Review;
+import com.example.doori.domain.User;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer>{
 	List<Review> findByMovieId(Integer movieId);
+	List<Review> findByUserId(User userId);
 }
